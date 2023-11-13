e7ac371d505dbb52f1084b04ef2920c5c4cd67ca
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-11-13 22:42:25 +0100

    fix: solve cors error

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,16 +1,5 @@
 const corsOptions = {
-  origin: (origin: any, cb: any) => {
-    const hostname = new URL(origin).hostname;
-    if (hostname === "localhost") {
-      //  Request from localhost will pass
-      cb(null, true);
-      return;
-    }
-    // Generate an error on other origins, disabling access
-    cb(new Error("Not allowed"), false);
-  },
-  methods: ["GET", "PUT", "POST", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: '*'
 };
 
 export default corsOptions;

67454f33db2d2d14cb13489e9dfc25ba5b506ffe
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-11-26 17:05:46 +0100

    feat: apply schema to blog endpoints

--- routes/blogs.ts
+++ routes/blogs.ts
@@ -1,91 +1,28 @@
 import { FastifyInstance } from 'fastify';
 import { getAllBlogs, getBlog, addBlog, updateBlog, deleteBlog } from '@/controller/blogs';
-import { getBlogSchema } from '@/schema/blog.schema';
-
-export const getBlogValidation = {
-  params: {
-    id: { type: 'string' },
-  },
-  response: {
-    200: {
-      type: 'object',
-      properties: {
-        id: { type: 'integer' },
-        title: { type: 'string' },
-      },
-    },
-  },
-};
-
-const addBlogValidation = {
-  body: {
-    type: 'object',
-    required: ['title'],
-    properties: {
-      title: { type: 'string' },
-    },
-  },
-  response: {
-    200: {
-      type: 'object',
-      properties: {
-        id: { type: 'integer' },
-        title: { type: 'string' },
-      },
-    },
-  },
-};
-
-const updateBlogValidation = {
-  params: {
-    id: { type: 'string' },
-  },
-  body: {
-    type: 'object',
-    required: ['title'],
-    properties: {
-      title: { type: 'string' },
-    },
-  },
-  response: {
-    201: {
-      type: 'object',
-      properties: {
-        message: { type: 'string' },
-      },
-    },
-  },
-};
-const deleteBlogValidation = {
-  params: {
-    id: { type: 'string' },
-  },
-  response: {
-    201: {
-      type: 'object',
-      properties: {
-        id: { type: 'integer' },
-        title: { type: 'string' },
-      },
-    },
-  },
-};
+import {
+  deleteBlogSchema,
+  getAllBlogSchema,
+  getBlogSchema,
+  postBlogSchema,
+  updateBlogSchema,
+} from '@/schema/blog.schema';
 
 async function routes(fastify: FastifyInstance) {
   // List all blogs, paginated
-  fastify.get('/', getAllBlogs);
+  fastify.get('/', { schema: getAllBlogSchema }, getAllBlogs);
 
   // Get one blog
   fastify.get('/:id', { schema: getBlogSchema }, getBlog);
 
   // Deleteing a blog
-  fastify.delete('/:id', { schema: deleteBlogValidation }, deleteBlog);
+  fastify.delete('/:id', { schema: deleteBlogSchema }, deleteBlog);
 
   // Create a new blog
-  fastify.post('/', { schema: addBlogValidation }, addBlog);
+  fastify.post('/', { schema: postBlogSchema }, addBlog);
 
   // Update an existing Category
-  fastify.put('/:id', { schema: updateBlogValidation }, updateBlog);
+  fastify.put('/:id', { schema: updateBlogSchema }, updateBlog);
 }
 
 export default routes;

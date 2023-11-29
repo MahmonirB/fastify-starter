6ed49a43e8db45e00aa568ab8bde1580edc72271
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-11-29 14:17:51 +0100

    feat: dockerize app

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,19 +1,23 @@
 const schema = {
-  type: "object",
-  required: ["PORT"],
+  type: 'object',
+  required: ['PORT', 'DATABASE_URL'],
   properties: {
     PORT: {
-      type: "string",
+      type: 'string',
       default: 3000,
+    },
+    DATABASE_URL: {
+      type: 'string',
+      default: 'Salam azizam',
     },
   },
 };
 
 const envOptions = {
-  confKey: "config", // optional, default: 'config'
+  confKey: 'config', // optional, default: 'config'
   schema: schema,
   dotenv: {
-    path: `.env.${process.env.NODE_ENV}`,
+    path: `./.env.${process.env.NODE_ENV}`,
     debug: true,
   },
 };

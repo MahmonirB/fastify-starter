c5144faf551f2fe89448382ff04b6f475ae287cf
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-12-10 22:48:42 +0100

    fix: improve docker file and build

--- dist/schema/common.schema.js
+++ dist/schema/common.schema.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.paramIdSchema = exports.messageSchema = void 0;
+// Just a single response object including a message
 exports.messageSchema = {
     $id: 'messageResponseSchema',
     type: 'object',
@@ -8,6 +9,7 @@ exports.messageSchema = {
         message: { type: 'string' },
     },
 };
+// Used to validate/match URLS that include an ':id' param
 exports.paramIdSchema = {
     $id: 'paramIdSchema',
     type: 'object',
